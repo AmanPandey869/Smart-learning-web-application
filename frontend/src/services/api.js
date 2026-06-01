78c5a45e5a616fcc4b42d1fa82cbd5590e492392
Aman Pandey <amanpandey@Amans-MacBook-Air.local>
2026-06-01 15:05:12 +0530

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://smart-learning-web-application.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
